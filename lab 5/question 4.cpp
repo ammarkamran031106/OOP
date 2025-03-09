0e089e3435f02348bb21f4a73ced7ce5d790bdac
Ammar kamran ali <ammarkamran2006@gmail.com>
2025-03-09 14:11:21 +0500

    Update question 4.cpp

--- lab 5/question 4.cpp
+++ lab 5/question 4.cpp
@@ -3,12 +3,18 @@ using namespace std;
 class BankAccount{
 	int accountNumber;
 	string accountHolderName;
-	static int balance;
+	int balance;
 	public:
 		BankAccount(int a,string b,int c){
 			accountNumber=a;
 			accountHolderName=b;
 			balance=c;
+		}
+		int get_balance(){
+			return balance;
+		}
+		void set_balance(int s){
+			balance=s;
 		}
 		void setaccountNumber(int a){
 			accountNumber=a;
@@ -35,14 +41,27 @@ class BankAccount{
 		void display(){
 			 cout<<"Account Number:"<<getaccountNumber()<<endl;
 			 cout<<"Account Holder Name:"<<getaccountHolderName()<<endl;
-			 cout<<"Balance:"<<balance<<endl;
+			 cout<<"Balance:"<<get_balance()<<endl;
 		}
 };
-int BankAccount::balance=0;
 int main(){
-	BankAccount employee(1442422,"owais",40000);
-	employee.deposit(100000);
-	employee.withdraw(30000);
-	employee.deposit(55000);
-	employee.display();
+	BankAccount accounts[3] = {
+    BankAccount(0000000, "Ammar", 5000),
+    BankAccount(0000001, "Owais", 3400),
+    BankAccount(0000002, "Saqib", 4000)};
+	for(int i=0;i<3;i++){
+		accounts[i].display();
+	}
+	accounts[0].deposit(500);
+	accounts[1].deposit(1000);
+	accounts[2].deposit(700);
+	for(int i=0;i<3;i++){
+		accounts[i].display();
+	}
+	accounts[0].withdraw(200);
+	accounts[1].withdraw(700);
+	accounts[2].withdraw(400);
+	for(int i=0;i<3;i++){
+		accounts[i].display();
+	}
 }
