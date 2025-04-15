88d0e4fb17823a5cc091c6bedabd43253043bd1b
Ammar kamran ali <ammarkamran2006@gmail.com>
2025-04-15 15:08:26 +0500

    Update main.cpp

--- Lab 8/Question 03/main.cpp
+++ Lab 8/Question 03/main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main(){
 	Book b("Ammar Kamran", "Sameed", "892232534");
+	Library L;
 	cout << b.getAuthor() << endl;
+	cout << b.getISBN() << endl;
+	cout << b.getTitle() << endl;
+	L.addBook();
+	L.removeBook();
+	L.searchBook();
+	
 }
